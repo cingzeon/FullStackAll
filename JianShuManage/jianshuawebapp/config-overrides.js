44e7ca25a6c9c66d3eea04a1b6127cadac817a31
Alan <loungcingzeon@sina.com>
2019-03-04 00:14:25 +0800

    简书web笔记

---
+++ JianShuManage/jianshuawebapp/config-overrides.js
@@ -0,0 +1,11 @@
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#1DA57A' },
+    }),
+);
